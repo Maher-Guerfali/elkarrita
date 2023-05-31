915843a6962980128c2523576bb932dfed98054d
Maher Guerfali <47715285+Maher-Guerfali@users.noreply.github.com>
2023-05-31 14:09:47 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-     El Karrita coming soon...
+     El Karrita game coming soon...
+        
       </div>
     </main>
   )
